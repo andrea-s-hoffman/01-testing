d167fc374ce3063e0f006c4f7bfa2645a1a071cc
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2022-10-20 08:02:34 -0400

    updates

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -2,17 +2,26 @@ const fizzbuzz = require("../src/fizzbuzz");
 
 // Numbers divisible by 3 should return the word "Fizz", numbers divisible by 5 should return the word "Buzz", numbers divisible by both 3 and 5 should return the word "FizzBuzz", and numbers that are not divisible by 3 or 5 should return the number itself.
 
-//a describe block groups together tests for a specific feature
+// "Red, Green, Refactor!" ---------------------
+// - Red: write a failing test
+// - Green: make that test pass
+// - Refactor: clean up code
+
+// ---------------------------------------------
+
+// a describe block groups together tests for a specific feature
 describe("FizzBuzz function", () => {
   // here's a test for Fizz:
   test("number divisible  by 3", () => {
-    //call function with a number divisible by 3. Store the return value equal in a variable called "result".
+    // call function with a number divisible by 3. Store the return value equal in a variable called "result".
     let result = fizzbuzz(9);
-    //assert (or check) that the result is correct
+    // assert (or check) that the result is correct
     expect(result).toBe("Fizz");
   });
-  //   write a test for Buzz:
+  // ---------------------------------------------
+  // Once your Fizz test passes, write a test for Buzz:
   // ...
-  // write a test for FizzBuzz:
+  // ---------------------------------------------
+  // Once your Buzz test passes, write a test for FizzBuzz:
   // ...
 });
